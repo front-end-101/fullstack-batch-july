9787612af720c382a4530920d4ce51668c44b885
react faculty <reactjs@fusion-institute.com>
2024-02-13 11:59:30 +0530

    js

---
+++ js/app.js
@@ -0,0 +1,97 @@
+// // console.log(message)
+// // var message = "hello";
+// // var message = "World";
+// // console.log(message);
+
+// // console.log(message);
+
+// // console.log(mes1)
+// // let mes1 = "Hello let";
+// // mes1 = "Hello let 2";
+// // console.log(mes1)
+
+// // console.log(mes2)
+// // const mes2 = "Hey World!";
+// // mes2= "Hello";
+// // console.log(mes2)
+
+// // hoisting
+
+//                     // var    let    const
+// // reassignment       yes      yes    no
+// // redeclare            yes   no     no
+// // scope           functional block  block
+
+
+
+// // function abc (){
+// //     let firstname = "john";
+// // }
+// // console.log(firstname)
+
+
+// // const istrue = true;
+
+// // if(istrue){
+// //     const lastName = "Cena";
+// // }
+// // console.log(lastName)
+
+// // let,const - block(if-else,function,{})
+// //var - functional scope
+
+// // string
+// const greet = "Hello";
+// console.log(typeof greet)
+
+// const num = 12;
+// console.log(typeof num)
+
+// const isTrue = true;
+// console.log(isTrue);
+
+// // undefined
+
+// // null
+
+// // object
+
+// const obj = {}
+
+// console.log(typeof obj)
+
+// // function in js
+
+// //1) regular function
+
+// // function add(num1=0,num2=0){
+
+// //     return num1 + num2;
+// // }
+// // console.log(add(4,8))
+
+// // Arrow function
+// const add = (num1=0,num2=0) => num1 + num2;
+    
+// console.log(add(4,8))
+
+// // console.log(mulitply(23,90))
+    
+// // anonymous functions
+// const fn = function(n1,n2){
+//     return n1 / n2;
+// }
+// console.log(fn(200,43))
+
+//(IIFE) Immediatly invoked fucntion expression
+
+(() => {
+    console.log("IIFE")
+})()
+
+
+
+
+
+
+
