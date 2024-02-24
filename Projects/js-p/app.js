028120a108d6b1ca252e64dacc0412bc6081a4d4
react faculty <reactjs@fusion-institute.com>
2024-02-24 13:06:07 +0530

    React

--- Projects/js-p/app.js
+++ Projects/js-p/app.js
@@ -1,27 +1,51 @@
-const url = "https://jsonplaceholder.typicode.com/users";
+// const url = "https://jsonplaceholder.typicode.com/users";
 
-const fetchData = fetch(url)
-// pending - intital
-// success- then
-// fail - catch
+// const fetchData = fetch(url,{
+//     method: "POST",
+//     body: {},
+//     headers: {
+//         Authorization: `Bearer ${token}`
+//     }
+// });
 
-fetchData
-.then((res) => res.json())
-.then(data => console.log(data))
-.catch((err) => console.log(err))
+// // console.log(fetchData)
 
-
-async function fetchDataFn (url){
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data);
-}
-
-fetchDataFn(url)
+// // fetchData
+// //     .then((res) => res.json())
+// //     .then(data => printData(data))
+// //     .catch(err => console.log(err))
+// //     .finally(() => console.log("FINALLY"))
 
 
+// async function fetchDataFn() {
+//     try {
+//         const res = await fetch(url);
+//         const data = await res.json();
+//         console.log(data)
+//         printData(data)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
 
+// fetchDataFn()
 
+// function printData(data) {
+//     data.forEach((item) => {
+//         const el = document.createElement("p");
+//         // el.innerText = item.name
+//         el.innerHTML = `
+//         <div class="card">
+//         <h1>${item.name}</h1>
+//         <p>${item.email}<p>
+//         </div>
+//         `
+//         document.body.appendChild(el);
+//     })
+// }
+
+// object
+// state - pending fulfill reject
 
 
 
@@ -56,3 +80,68 @@ fetchDataFn(url)
 //     })
 // }
 
+
+// function outer (){
+//     let count =0;
+
+//     return () => {
+//         count++;
+//         console.log(count)
+//     }
+// }
+
+// const innerfn = outer()
+// innerfn()
+
+
+
+// console.log(this)
+
+// function abc(){
+//     console.log(this);
+// }
+
+// abc()
+
+
+// const obj = {
+//     name: "Jack",
+//     print(){
+//         console.log(this.name)
+//     }
+// }
+
+// obj.print()
+
+
+
+
+const John = {
+    name: "John",
+    city: "Pune"
+}
+
+const Jenny = {
+    name: "Jenny",
+    city: "Mumbai"
+}
+
+function printName(greeting){
+    console.log(greeting, this.name);
+}
+
+
+printName()
+
+printName.call(Jenny, "Hello")
+
+printName.apply(John, ["Hey"])
+
+const ObjJenny = printName.bind(Jenny, "Hello from bind");
+
+ObjJenny()
+
+
+
+
+localStorage.setItem("name", "Fusion");
